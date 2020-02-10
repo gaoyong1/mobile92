9274436b8c1f1032025c423eadd33a581c85fb3c
gaoyong1 <243804968@qq.com>
2020-02-10 20:40:11 +0800

    初始化

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 配置用户账户信息成员
+    user: JSON.parse(localStorage.getItem('userinfo'))
   },
   mutations: {
+    // 修改用户信息
+    updateUser: function (state, data) {
+      // 1. vuex的共享数据要修改
+      state.user = data
+      // 2. localStorage数据要修改
+      localStorage.setItem('userinfo', JSON.stringify(data))
+    },
+    clearUser: function (state, data) {
+      state.user = data
+      localStorage.clear()
+    }
+
   },
   actions: {
   },
